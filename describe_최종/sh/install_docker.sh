bc2ab58adcc7efedd8fec36fde220793e2d7e118
JungJiwone <128367048+JungJiwone@users.noreply.github.com>
2023-06-20 17:37:10 +0900

    Update install_docker.sh

--- describe_최종/sh/install_docker.sh
+++ describe_최종/sh/install_docker.sh
@@ -9,7 +9,15 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt update
 
 # docker package install
-apt install docker-ce docker-ce-cli containerd.io -y
+# apt install docker-ce docker-ce-cli containerd.io -y
+# docker 저장소 추가 : docker 패키지 설치하기 위해 docker 저장소를 추가합니다. 다음 명령을 사용하여 docker 저장소를 추가한다.
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# 패지지 업데이트 : 패키지 목록을 업데이트하고 docker 설치
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
 
 # docker daemon start and enable
 systemctl enable docker
