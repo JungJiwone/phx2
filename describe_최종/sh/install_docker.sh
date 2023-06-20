5cae573bc92e3288eb68f0e23f5894756e4a0cf9
JungJiwone <128367048+JungJiwone@users.noreply.github.com>
2023-06-20 11:33:01 +0900

    Create install_docker.sh

---
+++ describe_최종/sh/install_docker.sh
@@ -0,0 +1,16 @@
+# docker package apt-key download and add
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
+
+# set docker key
+apt-key fingerprint OEBFCD88
+
+# docker repository add and update
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+
+# docker package install
+apt install docker-ce docker-ce-cli containerd.io -y
+
+# docker daemon start and enable
+systemctl enable docker
+systemctl start docker
