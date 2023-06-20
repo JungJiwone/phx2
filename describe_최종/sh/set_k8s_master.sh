c81e378180a0475335c5e5a1ece68dbfb8b67a41
JungJiwone <128367048+JungJiwone@users.noreply.github.com>
2023-06-20 12:15:41 +0900

    Update set_k8s_master.sh

--- describe_최종/sh/set_k8s_master.sh
+++ describe_최종/sh/set_k8s_master.sh
@@ -30,15 +30,15 @@ echo $(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin 
 apt install -y expect
 
 expect << EOF
-spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@10.10.13.3:/home/phoenix/token.txt
+spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@20.20.50.2:/home/phoenix/token.txt
 expect "password:" {send "VMware1!\r"}
 expect eof
 
-spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@10.10.13.4:/home/phoenix/token.txt
+spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@20.20.50.3:/home/phoenix/token.txt
 expect "password:" {send "VMware1!\r"}
 expect eof
 
-spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@10.10.13.5:/home/phoenix/token.txt
+spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@20.20.50.4:/home/phoenix/token.txt
 expect "password:" {send "VMware1!\r"}
 expect eof
 EOF
