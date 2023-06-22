4f36bad9285db28cf2d12217d74229038e8c17a6
JungJiwone <128367048+JungJiwone@users.noreply.github.com>
2023-06-22 09:42:05 +0900

    Update 21-1_set_k8s_worker_03.sh

---
+++ describe_최종/21-1_set_k8s_worker_03.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+su - << EOF
+VMware1!
+hostnamectl set-hostname k8s-worker-03
+cat /home/phoenix/set_net.yml > /etc/netplan/01-network-manager-all.yaml
+netplan apply
+echo "127.0.0.1 k8s-worker-03" >> /etc/hosts
+EOF
